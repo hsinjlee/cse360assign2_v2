99ee040923fbf18dcb5aced4635bbf16881beadb
hsinjlee <rlee4408@gmail.com>
2019-02-25 12:39:50 -0700

    Create the third version

--- CSE360a2v2/src/cse360assign2/Calculator.java
+++ CSE360a2v2/src/cse360assign2/Calculator.java
@@ -1,25 +1,28 @@
-
 package cse360assign2;
 
 /**
  * Student name : Hsin-Jung Lee
  * Class ID: 451
  * Assignment 2
- * Description: This program will calculator numbers.
+ * Description: This program will calculator numbers and print history.
+ * https://github.com/hsinjlee/cse360assign2_v2.git
  */
 
 /**
- * class Calculator contains integer total and calculator numbers.
+ * class Calculator contains a private integer total, a private StringBuffer
+ * callHistory,and calculator numbers.
  */
 public class Calculator {
 
     private int total;
+    private StringBuffer calHistory = new StringBuffer();
 
     /**
      * class constructor
      */
     public Calculator() {
         total = 0;
+        calHistory.append("0");
     }
 
     /**
@@ -32,34 +35,39 @@ public class Calculator {
     }
 
     /**
-     * add method add integer value
+     * add method add integer value and append " + " sign and value
      *
      * @param value
      */
     public void add(int value) {
         total = total + value;
+        calHistory.append(" + " + value);
     }
 
     /**
-     * subtract method subtract integer value
+     * subtract method subtract integer value and append " - " sign and value
      *
      * @param value
      */
     public void subtract(int value) {
         total = total - value;
+        calHistory.append(" - " + value);
     }
 
     /**
-     * multiply method multiply integer value
+     * multiply method multiply integer value and append " * " sign and value
      *
      * @param value
      */
     public void multiply(int value) {
         total = total * value;
+        calHistory.append(" * " + value);
     }
 
     /**
-     * divide method divide integer value
+     * divide method divide integer value and append " / " sign and value.
+     * It only shows integer division
+     * if value equals 0, total will be 0;
      *
      * @param value
      */
@@ -67,16 +75,17 @@ public class Calculator {
         if (value == 0) {
             total = 0;
         } else {
-            total = (int) (total / value);
+            total = (total / value);
         }
+        calHistory.append(" / " + value);
     }
 
     /**
      * getHistory method get history data
      *
-     * @return String history
+     * @return String calHistory
      */
     public String getHistory() {
-        return "";
+        return this.calHistory.toString();
     }
 }
