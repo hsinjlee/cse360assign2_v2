6a5784b29f0076b55ab9f36e852a7f0c6830a79f
hsinjlee <rlee4408@gmail.com>
2019-02-25 12:26:26 -0700

    Create the initial version

---
+++ CSE360a2v2/src/cse360assign2/Calculator.java
@@ -0,0 +1,78 @@
+
+package cse360assign2;
+
+/**
+ * Student name : Hsin-Jung Lee
+ * Class ID: 451
+ * Assignment 2
+ * Description: This program will calculator numbers.
+ */
+
+/**
+ * class Calculator contains integer total and calculator numbers.
+ */
+public class Calculator {
+
+    private int total;
+
+    /**
+     * class constructor
+     */
+    public Calculator() {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * getTotal method get total
+     *
+     * @return integer total
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     * add method add integer value
+     *
+     * @param value
+     */
+    public void add(int value) {
+
+    }
+
+    /**
+     * subtract method subtract integer value
+     *
+     * @param value
+     */
+    public void subtract(int value) {
+
+    }
+
+    /**
+     * multiply method multiply integer value
+     *
+     * @param value
+     */
+    public void multiply(int value) {
+
+    }
+
+    /**
+     * divide method divide integer value
+     *
+     * @param value
+     */
+    public void divide(int value) {
+
+    }
+
+    /**
+     * getHistory method get history data
+     *
+     * @return String history
+     */
+    public String getHistory() {
+        return "";
+    }
+} 
